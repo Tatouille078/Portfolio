f272617eb0ef2bbaa25ebb8af9aa07127a31c585
Tatouille078 <tatouille.loick@gmail.com>
2024-12-29 02:08:44 +0100

    Did accessibility and finished js and css for the about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header className={`flex justify-center items-center min-h-20 text-xl transition-all duration-1000 ${isTextEvent ? 'w-[100vh] gap-16 border-b-2 text-slate-100' : 'w-0 gap-0 border-b-2 text-zinc-900'}`}>
+    <header className={`flex justify-center items-center min-h-[7vh] text-xl transition-all duration-1000 ${isTextEvent ? 'w-[50vw] gap-16 border-b-2 text-slate-100' : 'w-0 gap-0 motion-reduce:gap-16 motion-reduce:w-[50vw] border-b-2 motion-reduce:text-slate-100 text-zinc-900'}`}>
       <p>About</p>
       <p>Contact</p>
       <p>Projects</p>
