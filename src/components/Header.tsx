f1b2311bdb5d86ca82a6adaca6c581a62dd27959
Tatouille078 <tatouille.loick@gmail.com>
2025-01-31 22:30:47 +0100

    Changed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation } from 'react-router-dom';
 
 const Header: React.FC = () => {
@@ -9,28 +9,23 @@ const Header: React.FC = () => {
   const isContactPage = location.pathname === "/contact";
   const isProjectPage = location.pathname.startsWith("/project/");
   const [isTextEvent, setTextEvent] = useState(false);
+  const [isHoverSchool, setHoverSchool] = useState(false);
 
   useEffect(() => {
-          const timeout = setTimeout(() => {
-              setTextEvent(true);
-          }, 1300);
-  
-          return () => clearTimeout(timeout);
-      }, []);
+    const timeout = setTimeout(() => {
+      setTextEvent(true);
+    }, 1300);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+
 
   return (
-    <header className={`flex justify-center items-center min-h-[7vh] text-xl transition-all gap-8 border-b-2 w-[50vw] ${isHomePage ? "fade-header" : ""} ${isTextEvent || isContactPage || isProjectPage ? "pointer-events-auto" : "pointer-events-none"}`}>
+    <header className={`flex justify-center items-center h-[7vh] text-xl transition-all gap-[3vw] border-b-2 w-[50vw] ${isHomePage ? "fade-header" : ""} ${isTextEvent || isContactPage || isProjectPage ? "pointer-events-auto" : "pointer-events-none"}`}>
       <Link to="/">
         <div className="relative group flex flex-col w-24 items-center">
-          <p className={`z-10 transition-opacity ${ isHomePage && isTextEvent ? "text-[#ffd59e] underline transition-colors duration-1000" : "" } ${ isTextEvent || isContactPage || isProjectPage ? "mix-blend-difference opacity-100" : "opacity-0" }`}>About</p>
-          <span className="absolute h-7 headerHover1 transition-all w-0 group-hover:w-full group-hover:-translate-x-6 duration-[350ms]"></span>
-          <span className="absolute h-7 headerHover2 transition-all w-0 group-hover:w-full duration-[250ms]"></span>
-          <span className="absolute h-7 headerHover3 transition-all w-0 group-hover:w-full group-hover:translate-x-6"></span>
-        </div>
-      </Link>
-      <Link to="/contact">
-        <div className="relative group flex flex-col w-24 items-center">
-          <p className={`z-10 transition-opacity ${ isContactPage ? "text-[#ffd59e] underline" : "" } ${ isTextEvent || isContactPage || isProjectPage ? "mix-blend-difference opacity-100" : "opacity-0" }`}>Contact</p>
+          <p className={`z-10 transition-opacity ${isHomePage && isTextEvent ? "text-[#ffd59e] underline transition-colors duration-1000" : ""} ${isTextEvent || isContactPage || isProjectPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>About</p>
           <span className="absolute h-7 headerHover1 transition-all w-0 group-hover:w-full group-hover:-translate-x-6 duration-[350ms]"></span>
           <span className="absolute h-7 headerHover2 transition-all w-0 group-hover:w-full duration-[250ms]"></span>
           <span className="absolute h-7 headerHover3 transition-all w-0 group-hover:w-full group-hover:translate-x-6"></span>
@@ -38,7 +33,37 @@ const Header: React.FC = () => {
       </Link>
       <Link to="/project/SkyFlippy">
         <div className="relative group flex flex-col w-24 items-center">
-          <p className={`z-10 transition-opacity ${ isProjectPage ? "text-[#ffd59e] underline" : "" } ${ isTextEvent || isProjectPage || isContactPage ? "mix-blend-difference opacity-100" : "opacity-0" }`}>Projects</p>
+          <p className={`z-10 transition-opacity ${isProjectPage ? "text-[#ffd59e] underline" : ""} ${isTextEvent || isProjectPage || isContactPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>Projects</p>
+          <span className="absolute h-7 headerHover1 transition-all w-0 group-hover:w-full group-hover:-translate-x-6 duration-[350ms]"></span>
+          <span className="absolute h-7 headerHover2 transition-all w-0 group-hover:w-full duration-[250ms]"></span>
+          <span className="absolute h-7 headerHover3 transition-all w-0 group-hover:w-full group-hover:translate-x-6"></span>
+        </div>
+      </Link>
+      <div className="relative w-24" onMouseEnter={() => setHoverSchool(true)} onMouseLeave={() => setHoverSchool(false)} onClick={() => setHoverSchool(true)}>
+        <div className="relative group flex flex-col items-center cursor-default">
+          <p className={`z-10 transition-opacity ${isProjectPage ? "text-[#ffd59e] underline" : ""} ${isTextEvent || isProjectPage || isContactPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>School</p>
+          <span className="absolute h-7 headerHover4 transition-all w-0 group-hover:w-full group-hover:-translate-x-6 duration-[350ms]"></span>
+          <span className="absolute h-7 headerHover5 transition-all w-0 group-hover:w-full duration-[250ms]"></span>
+          <span className="absolute h-7 headerHover6 transition-all w-0 group-hover:w-full group-hover:translate-x-6"></span>
+        </div>
+        <div className={`absolute pt-[5vh] top-0 text-center flex flex-col items-center justify-center right-0 left-0 text-zinc-200 ${isHoverSchool ? "" : "pointer-events-none"}`}>
+          <div className={`relative group h-14 flex justify-center items-center border-b-2 border-zinc-300 transition-all duration-500 ${isHoverSchool ? "opacity-100 w-[70%]" : "opacity-0 -translate-y-4 w-0"}`}>
+            <p className={`z-10 transition-opacity ${isProjectPage ? "text-[#ffd59e] underline" : ""} ${isTextEvent || isProjectPage || isContactPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>apagnan</p>
+            <span className="absolute h-7 headerHover4 transition-all w-0 group-hover:w-[120%] group-hover:-translate-x-6 duration-[350ms]"></span>
+            <span className="absolute h-7 headerHover5 transition-all w-0 group-hover:w-[120%] duration-[250ms]"></span>
+            <span className="absolute h-7 headerHover6 transition-all w-0 group-hover:w-[120%] group-hover:translate-x-6"></span>
+          </div>
+          <div className={`relative group h-14 flex justify-center items-center border-b-2 border-zinc-300 transition-all duration-500 ${isHoverSchool ? "opacity-100 w-[70%]" : "opacity-0 -translate-y-8 w-0"}`}>
+            <p className={`z-10 transition-opacity ${isProjectPage ? "text-[#ffd59e] underline" : ""} ${isTextEvent || isProjectPage || isContactPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>quoicoubeh</p>
+            <span className="absolute h-7 headerHover4 transition-all w-0 group-hover:w-[160%] group-hover:-translate-x-6 duration-[350ms]"></span>
+            <span className="absolute h-7 headerHover5 transition-all w-0 group-hover:w-[160%] duration-[250ms]"></span>
+            <span className="absolute h-7 headerHover6 transition-all w-0 group-hover:w-[160%] group-hover:translate-x-6"></span>
+          </div>
+        </div>
+      </div>
+      <Link to="/contact">
+        <div className="relative group flex flex-col w-24 items-center">
+          <p className={`z-10 transition-opacity ${isContactPage ? "text-[#ffd59e] underline" : ""} ${isTextEvent || isContactPage || isProjectPage ? "mix-blend-difference opacity-100" : "opacity-0"}`}>Contact</p>
           <span className="absolute h-7 headerHover1 transition-all w-0 group-hover:w-full group-hover:-translate-x-6 duration-[350ms]"></span>
           <span className="absolute h-7 headerHover2 transition-all w-0 group-hover:w-full duration-[250ms]"></span>
           <span className="absolute h-7 headerHover3 transition-all w-0 group-hover:w-full group-hover:translate-x-6"></span>
