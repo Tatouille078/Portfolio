ddea4f91d7b0b6f4f399e8a929541ef33f0ab8db
Tatouille078 <tatouille.loick@gmail.com>
2025-06-25 23:39:06 +0200

    GIthub pages is amazing AGAIN

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Project from './pages/Project';
@@ -8,15 +8,13 @@ import Docs from './pages/Docs';
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/project/:name" element={<Project />} />
-        <Route path="/journey" element={<Journey />} />
-        <Route path="/docs" element={<Docs />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/project/:name" element={<Project />} />
+      <Route path="/journey" element={<Journey />} />
+      <Route path="/docs" element={<Docs />} />
+    </Routes>
   )
 }
 
